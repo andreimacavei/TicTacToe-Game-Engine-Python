64eff24691f1c2ecd18193eff9bfb0d54f8f87f7
Andrei MG <andrei_gabriel.macavei@cti.pub.ro>
2013-03-09 13:14:41 +0200

    RED: test_that_a_player_cannot_move_into_an_already_occupied_board_cell

---
+++ test_harness.py
@@ -0,0 +1,19 @@
+import unittest
+
+class TicTacToeTestSuite(unittest.TestCase):
+
+    def test_that_a_player_cannot_move_into_an_already_occupied_board_cell(self):
+        # define_input
+        game_state = {}
+        game_state['owned_by_x'] = ["c3"]
+        game_state['owned_by_zero'] = []
+        game_state['who_moves_next'] = 0
+        game_state['last_move_was_valid'] = True
+
+        next_move = "c3"
+        
+        # assert
+        self.assertFalse(game_state['last_move_was_valid'])
+        
+unittest.main()
+
