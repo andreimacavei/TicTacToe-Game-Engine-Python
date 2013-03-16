9b0463609cec1e2018f79f291493dbf98e9a2ee3
Andrei MG <andrei_gabriel.macavei@cti.pub.ro>
2013-03-16 16:40:48 +0200

    REFACTOR: removed trailing white spaces

--- test_harness.py
+++ test_harness.py
@@ -11,16 +11,16 @@ class TicTacToeTestSuite(unittest.TestCase):
         game_state['last_move_was_valid'] = True
 
         next_move = 'c3'
-       
+
         # apply transformation
         occupied_board_positions = game_state['owned_by_x'] + game_state['owned_by_zero']
         game_state['last_move_was_valid'] = not next_move in occupied_board_positions
 
         # assert
         self.assertFalse(game_state['last_move_was_valid'])
-       
+
     def test_that_game_engine_correctly_verifies_consistency_1(self):
-       
+
         # define input
         game_state = {}
         game_state['owned_by_x'] = []
@@ -38,9 +38,9 @@ class TicTacToeTestSuite(unittest.TestCase):
 
         # assert
         self.assertEqual(why_the_game_ended_reason_id, 7)
-    
+
     def test_that_game_engine_correctly_verifies_consistency_2(self):
-        
+
         # define input
         game_state = {}
         game_state['owned_by_x'] = ['a1']
@@ -60,19 +60,18 @@ class TicTacToeTestSuite(unittest.TestCase):
         self.assertEqual(why_the_game_ended_reason_id, 7)
 
     def test_that_game_engine_correctly_verifies_consistency_3(self):
-        
+
         # define input
         game_state = {}
         game_state['owned_by_x'] = []
         game_state['owned_by_zero'] = ['a2']
         why_the_game_ended_reason_id = 0
-       
+
         # apply_transformation
         size_of_owned_by_x = len(game_state['owned_by_x'])
         size_of_owned_by_zero = len(game_state['owned_by_zero'])
         if size_of_owned_by_x <  size_of_owned_by_zero:
             why_the_game_ended_reason_id = 8
-    
 
         # assert
         self.assertEqual(why_the_game_ended_reason_id, 8)
